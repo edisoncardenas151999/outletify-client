47cbc64ea8ffcc1f512492b60370e411990551e3
edisoncardenas151999 <108204103+edisoncardenas151999@users.noreply.github.com>
2022-12-03 14:27:33 -0500

    updated class to className

--- outletify-client/src/pages/LoginPage.js
+++ outletify-client/src/pages/LoginPage.js
@@ -44,84 +44,82 @@ function LoginPage(props) {
   return (
     <>
       <div className="LoginPage">
-        <div class="section">
-          <div class="container">
-            <div class="row full-height justify-content-center">
-              <div class="col-12 text-center align-self-center py-5">
-                <div class="section pb-5 pt-5 pt-sm-2 text-center">
-                  <div class="card-3d-wrap mx-auto">
-                    <div class="card-3d-wrapper">
-                      <div class="card-front">
-                        <div class="center-wrap">
-                          <div class="section text-center">
-                            <Link className="logo" to={"/"} Link>
+        <div className="section">
+          <div className="container">
+            <div className="row full-height justify-content-center">
+              <div className="col-12 text-center align-self-center py-5">
+                <div className="section pb-5 pt-5 pt-sm-2 text-center">
+                  <div className="card-3d-wrap mx-auto">
+                    <div className="card-3d-wrapper">
+                      <div className="card-front">
+                        <div className="center-wrap">
+                          <div className="section text-center">
+                            <Link classNameName="logo" to={"/"} Link>
                               <img
                                 src="https://images-platform.99static.com/kXV-v6IrH6Dgg41p5NGqdgbog3g=/100x100:900x900/500x500/top/smart/99designs-contests-attachments/118/118605/attachment_118605478"
                                 className="logo"
                               />
                             </Link>
                             <form onSubmit={handleLoginSubmit}>
-                              <div class="form-group mb-3">
+                              <div className="form-group mb-3">
                                 <input
                                   type="email"
                                   name="email"
-                                  class="form-style"
+                                  className="form-style"
                                   placeholder="Your Email"
                                   value={email}
                                   onChange={handleEmail}
                                 />
-                                <i class="input-icon uil uil-at"></i>
+                                <i className="input-icon uil uil-at"></i>
                               </div>
-                              <div class="form-group mt-5">
+                              <div className="form-group mt-5">
                                 <input
                                   type="password"
                                   name="password"
-                                  class="form-style"
+                                  className="form-style"
                                   value={password}
                                   onChange={handlePassword}
                                   placeholder="Your Password"
                                 />
-                                <i class="input-icon uil uil-lock-alt"></i>
+                                <i className="input-icon uil uil-lock-alt"></i>
                               </div>
-                              <button class="btn mt-4" type="submit">
+                              <button className="btn mt-4" type="submit">
                                 Login
                               </button>
                             </form>
                           </div>
                         </div>
                       </div>
-                      <div class="card-back">
-                        <div class="center-wrap">
-                          <div class="section text-center">
-                            <h4 class="mb-4 pb-3">Sign Up</h4>
-                            <div class="form-group">
+                      <div className="card-back">
+                        <div className="center-wrap">
+                          <div className="section text-center">
+                            <h4 className="mb-4 pb-3">Sign Up</h4>
+                            <div className="form-group">
                               <input
                                 type="text"
                                 name="logname"
-                                class="form-style"
+                                className="form-style"
                                 placeholder="Your Full Name"
-                                id="logname"
-                                autocomplete="off"
                               />
-                              <i class="input-icon uil uil-user"></i>
+                              <i className="input-icon uil uil-user"></i>
                             </div>
-                            <div class="form-group mt-2">
+                            <div className="form-group mt-2">
                               <input
                                 type="email"
                                 name="logemail"
-                                class="form-style"
+                                className="form-style"
                                 placeholder="Your Email"
                               />
-                              <i class="input-icon uil uil-at"></i>
+                              <i className="input-icon uil uil-at"></i>
                             </div>
-                            <div class="form-group mt-2">
+                            <div className="form-group mt-2">
                               <input
                                 type="password"
                                 name="logpass"
-                                class="form-style"
+                                className="form-style"
                                 placeholder="Your Password"
                               />
-                              <i class="input-icon uil uil-lock-alt"></i>
+                              <i className="input-icon uil uil-lock-alt"></i>
                             </div>
                           </div>
                         </div>
--- outletify-client/src/pages/SignupPage.js
+++ outletify-client/src/pages/SignupPage.js
@@ -37,16 +37,16 @@ function SignupPage(props) {
 
   return (
     <div className="SignupPage">
-      <div class="section">
-        <div class="container">
-          <div class="row full-height justify-content-center">
-            <div class="col-12 text-center align-self-center py-5">
-              <div class="section pb-5 pt-5 pt-sm-2 text-center">
-                <div class="card-3d-wrap mx-auto">
-                  <div class="card-3d-wrapper">
-                    <div class="card-front">
-                      <div class="center-wrap">
-                        <div class="section text-center">
+      <div className="section">
+        <div className="container">
+          <div className="row full-height justify-content-center">
+            <div className="col-12 text-center align-self-center py-5">
+              <div className="section pb-5 pt-5 pt-sm-2 text-center">
+                <div className="card-3d-wrap mx-auto">
+                  <div className="card-3d-wrapper">
+                    <div className="card-front">
+                      <div className="center-wrap">
+                        <div className="section text-center">
                           <Link className="logo" to={"/"} Link>
                             <img
                               src="https://images-platform.99static.com/kXV-v6IrH6Dgg41p5NGqdgbog3g=/100x100:900x900/500x500/top/smart/99designs-contests-attachments/118/118605/attachment_118605478"
@@ -54,75 +54,75 @@ function SignupPage(props) {
                             />
                           </Link>
                           <form onSubmit={handleSignupSubmit}>
-                            <div class="form-group mb-3">
+                            <div className="form-group mb-3">
                               <input
                                 type="email"
                                 name="email"
-                                class="form-style"
+                                className="form-style"
                                 placeholder="Your Email"
                                 value={email}
                                 onChange={handleEmail}
                               />
-                              <i class="input-icon uil uil-at"></i>
+                              <i className="input-icon uil uil-at"></i>
                             </div>
-                            <div class="form-group mt-5">
+                            <div className="form-group mt-5">
                               <input
                                 type="password"
                                 name="password"
-                                class="form-style"
+                                className="form-style"
                                 placeholder="Your Password"
                                 value={password}
                                 onChange={handlePassword}
                               />
-                              <i class="input-icon uil uil-lock-alt"></i>
+                              <i className="input-icon uil uil-lock-alt"></i>
                             </div>
-                            <div class="form-group mt-5">
+                            <div className="form-group mt-5">
                               <input
                                 type="text"
                                 name="name"
-                                class="form-style"
+                                className="form-style"
                                 placeholder="Your Name"
                                 value={name}
                                 onChange={handleName}
                               />
-                              <i class="input-icon uil uil-at"></i>
+                              <i className="input-icon uil uil-at"></i>
                             </div>
-                            <button class="btn mt-4" type="submit">
+                            <button className="btn mt-4" type="submit">
                               Signup
                             </button>
                           </form>
                         </div>
                       </div>
                     </div>
-                    <div class="card-back">
-                      <div class="center-wrap">
-                        <div class="section text-center">
-                          <div class="form-group">
+                    <div className="card-back">
+                      <div className="center-wrap">
+                        <div className="section text-center">
+                          <div className="form-group">
                             <input
                               type="text"
                               name="logname"
-                              class="form-style"
+                              className="form-style"
                               placeholder="Your Full Name"
                             />
-                            <i class="input-icon uil uil-user"></i>
+                            <i className="input-icon uil uil-user"></i>
                           </div>
-                          <div class="form-group mt-2">
+                          <div className="form-group mt-2">
                             <input
                               type="email"
                               name="logemail"
-                              class="form-style"
+                              className="form-style"
                               placeholder="Your Email"
                             />
-                            <i class="input-icon uil uil-at"></i>
+                            <i className="input-icon uil uil-at"></i>
                           </div>
-                          <div class="form-group mt-2">
+                          <div className="form-group mt-2">
                             <input
                               type="password"
                               name="logpass"
-                              class="form-style"
+                              className="form-style"
                               placeholder="Your Password"
                             />
-                            <i class="input-icon uil uil-lock-alt"></i>
+                            <i className="input-icon uil uil-lock-alt"></i>
                           </div>
                         </div>
                       </div>
