5953c974dd0e6679702c7873935faa2792896020
edisoncardenas151999 <108204103+edisoncardenas151999@users.noreply.github.com>
2022-12-03 14:02:23 -0500

    updated catergory links

--- outletify-client/src/Categories/Javascript.js
+++ outletify-client/src/Categories/Javascript.js
@@ -1,5 +1,33 @@
+import { useState, useEffect } from "react";
+
+import ItemCard from "../components/ItemCard";
+import axios from "axios";
+
 const Javascript = () => {
-  return <p>javascript</p>;
+  const [items, setItems] = useState(null);
+  const categoryId = "638b81adfd6737d69febe020";
+  const API_URL = "http://localhost:5005";
+  const getAllItems = () => {
+    const storedToken = localStorage.getItem("authToken");
+    axios
+      .get(`${API_URL}/auth/category/${categoryId}`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then((response) => setItems(response.data))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getAllItems();
+  }, []);
+
+  return (
+    <div className="item-container">
+      {items?.items?.map((item, index) => (
+        <ItemCard key={index} {...item} />
+      ))}
+    </div>
+  );
 };
 
 export default Javascript;
