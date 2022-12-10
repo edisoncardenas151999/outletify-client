b7e9710ce6ff338c37598e665d7877dfda71736d
edisoncardenas151999 <108204103+edisoncardenas151999@users.noreply.github.com>
2022-12-10 17:03:56 -0500

    updated shop

--- outletify-client/src/pages/Shop.js
+++ outletify-client/src/pages/Shop.js
@@ -46,8 +46,15 @@ const Shop = () => {
         </ul>
       </div>
       <div className="item-container">
-        {Items.map((item) => (
-          <ItemCard key={item._id} {...item} />
+        {Items.map((item, index) => (
+          <div className="item-page" key={index}>
+            <Link to={`/item/${item?._id}`}>
+              <strong className="item-name">{item?.name}</strong>
+              <br />
+              <img src={item?.img} alt="pic" />
+              <p>{`$${item?.price}`}</p>
+            </Link>
+          </div>
         ))}
       </div>
     </>
